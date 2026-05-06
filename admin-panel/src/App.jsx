71fa6791760fcd03e2f4608402d3b96b3fc78fb8
ishikapawar49-blip <ishikapawar49@gmail.com>
2026-05-07 00:51:20 +0530

    Contact form  section

--- admin-panel/src/App.jsx
+++ admin-panel/src/App.jsx
@@ -14,7 +14,7 @@ import AdminServices from "./pages/Admin/AdminServices";
 import AdminBookings from "./pages/Admin/AdminBookings";
 import AdminPayments from "./pages/Admin/AdminPayments";
 import AdminReviews from "./pages/Admin/AdminReviews";
-import Contacts from "./pages/Contacts/Contacts";
+import Contacts from "./pages/Admin/AdminContactQuery";
 import AdminNotifications from "./pages/Admin/AdminNotifications";
 import AdminSettings from "./pages/Admin/AdminSettings";
 
@@ -49,7 +49,7 @@ function App() {
         <Route path="bookings" element={<AdminBookings />} />
         <Route path="payments" element={<AdminPayments />} />
         <Route path="reviews" element={<AdminReviews />} />
-        <Route path="/admin/contacts"element={<Contacts />}/>
+        <Route path="contacts"element={<Contacts />}/>
         <Route path="notifications" element={<AdminNotifications />} />
         <Route path="settings" element={<AdminSettings />} />
       </Route>
